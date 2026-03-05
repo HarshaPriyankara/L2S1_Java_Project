9fd3929228f8a1f84e3f0366a4800aa2ac0581c2
Isuru Shadeep <shadeepisuru12345@gmail.com>
2026-03-05 15:29:35 +0530

    Add getCategory method to classify BMI

--- BMILogic.java
+++ BMILogic.java
@@ -11,3 +11,17 @@ public class BMILogic {
             return (weight * 703) / (height * height);
         }
     }
+
+
+    public static String getCategory(double bmi) {
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi < 25) {
+            return "Normal";
+        } else if (bmi < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+}
