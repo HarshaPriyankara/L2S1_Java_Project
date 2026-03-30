9df2ff198276528d943afe97c37bed6fba538d67
HarshaPriyankara <h9pharsha@gmail.com>
2026-03-30 22:49:53 +0530

    Add user,admin,lecturer,technicalofficer,udergraduate classes

---
+++ TechnicalOfficer.java
@@ -0,0 +1,11 @@
+public class TechnicalOfficer extends User{
+
+    private String officerID;
+
+    public void addAttendance(){}
+    public void updateAttendance(){}
+    public void addMedical(){}
+    public void updateMedical(){}
+    public void viewTimetable(){}
+    public void viewNotices(){}
+}
---
+++ Undergraduate.java
@@ -0,0 +1,18 @@
+public class Undergraduate extends User {
+
+    private String regNo;
+    private String batch;
+    private String studentType;
+    private boolean isRepeat;
+    private boolean isBatchMissed;
+
+    public void viewAttendance(){}
+    public void viewMedical(){}
+    public void addMedical(){}
+    public void viewGrades(){}
+    public void viewGPA(){}
+    public void viewTimetable(){}
+    public void viewNotices(){}
+    public void updateContactDetails(){}
+
+}
