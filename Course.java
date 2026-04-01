e34030a49512856cad57c27807c4c4d3dd24fda1
dilusharadeeshan <dilusharadeeshan@gmail.com>
2026-04-01 18:59:00 +0530

    Basic class structures

---
+++ Course.java
@@ -0,0 +1,26 @@
+public class Course {
+
+    Lecture lecture;
+
+    private String courseCode;
+    private String courseName;
+    private int credits;
+    private int theoryHours;
+    private int PracticalHours;
+    private String departmentid;
+
+
+    public void addMaterial(){
+
+    }
+
+    public void updateCourse(){
+
+    }
+
+    public String getCourseDetails(){
+
+
+    }
+
+}
---
+++ Marks.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Marks {
+    Grade grade;
+
+    private String markId;
+    private String regNo;
+    private String courseCode;
+    private String examType;
+    private String marksValue;
+    private String totalMarks;
+    private String sessionType;
+    private String uploadedBy;
+
+    public void uploadMarks(){
+
+    }
+
+    public double getCaMarks(){
+
+    }
+
+    public double getFinalMarks(){
+
+
+    }
+
+    public boolean checkEligibility(){
+
+    }
+
+    public List getBatchSummury(){
+
+    }
+
+
+}
