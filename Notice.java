375adf0b34513c74a3f47a708053c7ae19ff0e29
klynimsara <kulasinliyanageyasirunimsara@gmail.com>
2026-04-03 08:25:19 +0530

    Create Notice.java
    incomplete code

---
+++ Notice.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+class Notice {
+    private String noticeId;
+    private String title;
+    private String content;
+    private String createdBy;
+    private LocalDate createdDate;
+    private String targetRole;
+    private String departmentId;
+
+    private static List<Notice> noticeList = new ArrayList<>();
+
+
+    public Notice() 
+    }
+
+
+    public void createNotice() {
+        
+    }
+
+    public static void deleteNotice() {
+       
+    }
+
+
+    public void viewNotice() {
+       
+    }
+
+
+    public static List<Notice> viewAllNotice() {
+       
+    }
+}
