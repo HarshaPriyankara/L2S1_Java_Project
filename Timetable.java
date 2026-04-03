ffdbe6ace072cb7845504e18f30e15382a1f7f7e
klynimsara <kulasinliyanageyasirunimsara@gmail.com>
2026-04-03 08:28:41 +0530

    Create Timetable.java
    incomplete code

---
+++ Timetable.java
@@ -0,0 +1,41 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+class Timetable {
+    private String timeTableId;
+    private String courseCode;
+    private String departmentId;
+    private String day;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String venue;
+    private String sessionType;
+
+    private static List<Timetable> timetableList = new ArrayList<>();
+
+    public Timetable() {
+    
+    }
+
+
+    public void createTimeTable() {
+      
+    }
+
+    public void updateTimeTable() {
+     
+    }
+
+
+    public static void deleteTimeTable() {
+  
+    }
+
+    public static List<Timetable> getTimeTableByDept(String deptId) {
+        
+
+    public void display() {
+        
+    }
+}
