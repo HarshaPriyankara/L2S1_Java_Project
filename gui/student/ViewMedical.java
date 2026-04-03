a4a25a8a8e2237688117847dd54b909c7869360f
shadeep0918 <shadeepisuru12345@gmail.com>
2026-04-04 01:16:52 +0530

    view medical interface

---
+++ gui/student/ViewMedical.java
@@ -0,0 +1,34 @@
+package gui.student;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+
+public class ViewMedical extends JFrame {
+
+
+
+    public ViewMedical() {
+
+        JFrame frame = new JFrame();
+        setTitle("View Medicals");
+        setSize(650,700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        String[] columns ={"Serial Number","Reference Number","Medical Date","Medical Certificate Number","Submitted Date","Status"};
+
+       // DefaultTableModel model = new DefaultTableModel(data,columns);
+        setVisible(true);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        ViewMedical v = new ViewMedical();
+
+
+    }
+}
+
