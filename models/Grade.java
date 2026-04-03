7167c61649143a8567b100a4bf8e09745637c373
shadeep0918 <shadeepisuru12345@gmail.com>
2026-04-03 21:40:15 +0530

    create attendance,grade,medicalrecord classes

---
+++ models/Grade.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Grade {
+
+    private String gradeId;
+    private String regNo;
+    private String courseCode;
+    private int    credits;
+    private String grade;
+    private double gradePoint;
+    private String semester;
+
+
+    public Grade(String gradeId, String regNo, String courseCode, int credits, String semester) {
+        this.gradeId = gradeId;
+        this.regNo = regNo;
+        this.courseCode = courseCode;
+        this.credits = credits;
+        this.semester = semester;
+        this.grade =calculateGrade();
+        this.gradePoint =toGradePoint(this.grade);
+    }
+
+    private String calculateGrade() {
+        return "x";
+    }
+
+    private double toGradePoint(String grade) {
+        return 0;
+    }
+
+
+}
