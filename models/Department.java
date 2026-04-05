1c9a562820df49e74c6cae5c54256dc0bd6c3689
klynimsara <kulasinliyanageyasirunimsara@gmail.com>
2026-04-05 18:21:36 +0530

    Create Department.java

---
+++ models/Department.java
@@ -0,0 +1,19 @@
+public class Department {
+
+    private String departmentId;
+    private String departmentName;
+
+
+    public Department(){
+       
+    }
+
+    public String getDepartmentDetails() {
+      
+
+    }
+
+    public void updateDepartment() {
+       
+    }
+}
