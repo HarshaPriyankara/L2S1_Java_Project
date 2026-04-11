94436b2045d3b2172aa6bc860875a4fbc9f506df
shadeep0918 <shadeepisuru12345@gmail.com>
2026-04-11 08:53:30 +0530

    create view Notice panel inside the lecturer

--- GUI/lecturer/LecturerDashboard.java
+++ GUI/lecturer/LecturerDashboard.java
@@ -1,6 +1,7 @@
 package GUI.lecturer;
 
 import GUI.common.LoginForm;
+import GUI.common.ViewNotice;
 import javax.swing.*;
 import java.awt.*;
 
@@ -24,6 +25,7 @@ public class LecturerDashboard extends JFrame {
 
         contentPanel.add(buildHomePanel(),                          "Home");
         contentPanel.add(new ProfileManagementPanel(loggedInUserId), "Update Profile");
+        contentPanel.add(new ViewNotice("Lecturer",contentPanel,cardLayout), "View Notice Card");
         cardLayout.show(contentPanel, "Home");
     }
 
@@ -42,7 +44,7 @@ public class LecturerDashboard extends JFrame {
         sidebar.add(Box.createVerticalStrut(12));
         sidebar.add(navButton("Undergraduate Details", () -> cardLayout.show(contentPanel, "Undergraduate Details")));
         sidebar.add(Box.createVerticalStrut(12));
-        sidebar.add(navButton("View Notice", () -> cardLayout.show(contentPanel, "View Notice")));
+        sidebar.add(navButton("View Notice", () -> cardLayout.show(contentPanel, "View Notice Card")));
         sidebar.add(Box.createVerticalStrut(12));
         sidebar.add(navButton("Update Profile",       () -> cardLayout.show(contentPanel, "Update Profile")));
 
