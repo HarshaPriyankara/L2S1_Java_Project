d0c44cb6a70859251a95c86fec2905c671ea278f
dilusharadeeshan <dilusharadeeshan@gmail.com>
2026-04-22 07:28:30 +0530

    separete GUI and functions in stu

--- GUI/student/StudentDashboard.java
+++ GUI/student/StudentDashboard.java
@@ -21,7 +21,7 @@ public class StudentDashboard extends BaseDashboard {
     @Override
     protected void setupUserPanels() {
         // These classes (UpdateProfilePanel, AttendancePanel, etc.) should exist in your project
-        contentPanel.add(new UpdateProfilePanel(), "Update Profile");
+        contentPanel.add(new StudentProfilePanel(loggedInID), "Update Profile");
         contentPanel.add(new AttendancePanel(), "Attendance Details");
         contentPanel.add(new MedicalPanel(), "Medical Details");
         contentPanel.add(new CoursePanel(loggedInID), "Course Details");
